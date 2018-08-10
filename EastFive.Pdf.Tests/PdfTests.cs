d86daa6d7f909f3abbaa5327974ec815d3e86331
Keith Holloway <keith@eastfive.com>
2018-08-10 10:33:01 -0500

    Genesis

---
+++ EastFive.Pdf.Tests/PdfTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HtmlAgilityPack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EastFive.Pdf;
+using System.IO;
+
+namespace EastFive.Pdf.Tests
+{
+    [TestClass]
+    public class PdfTests
+    {
+        [TestMethod]
+        public void ConvertHtmlStringToPdf(string htmlString)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(htmlString);
+            var pdfStream = document.ToPDF();
+
+            using (var fileStream = File.Create("C:\\temp\\outputpdf.pdf"))
+            {
+                pdfStream.Seek(0, SeekOrigin.Begin);
+                pdfStream.CopyTo(fileStream);
+            }
+        }
+    }
+}
---
+++ EastFive.Pdf/PdfExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System;
+using TheArtOfDev.HtmlRenderer.PdfSharp;
+using HtmlAgilityPack;
+
+namespace EastFive.Pdf
+{
+    public static class PdfExtensions
+    {
+        public static Stream ConvertHtmlStringToPdf(this string htmlString)
+        {
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(htmlString);
+            var pdfStream = document.ToPDF();
+
+            //using (var fileStream = File.Create("C:\\temp\\outputpdf.pdf"))
+            //{
+            //    pdfStream.Seek(0, SeekOrigin.Begin);
+            //    pdfStream.CopyTo(fileStream);
+            //}
+            return pdfStream;
+        }
+
+        public static Stream ToPDF(this HtmlAgilityPack.HtmlDocument htmlDocument)
+        {
+            var config = new PdfGenerateConfig
+            {
+                PageSize = PdfSharp.PageSize.A4
+            };
+            config.SetMargins(20);
+
+            var doc = PdfGenerator.GeneratePdf(htmlDocument.ParsedText, config);
+
+            var output = new MemoryStream();
+            doc.Save(output);
+            return output;
+        }
+    }
+}
